93213469219f8411b87b1be338582f687ac957de
marybunde <marybunde@gmail.com>
2022-08-17 22:23:35 -0400

    lab done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,27 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        String selectUp = playList[startIndex];
+        String selectDown = playList[startIndex];
+        int upIndex = startIndex;
+        int downIndex = startIndex;
+        int clicks = 0;
+        int lastIndex = this.playList.length - 1;
+
+        while (!selectUp.equals(selection) && !selectDown.equals(selection)) {
+            upIndex = (upIndex == 0) ? lastIndex : upIndex - 1;
+            downIndex = (downIndex == lastIndex) ? 0 : downIndex + 1;
+            clicks++;
+            selectUp = playList[upIndex];
+            selectDown = playList[downIndex];
+        }
+
+        return clicks;
     }
 }
